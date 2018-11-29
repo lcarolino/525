4ddcfb695c872f0f065568bbf5e0e06802f269f8
Luciano <luciano@dexter.com.br>
2018-11-29 12:28:54 +0000

    arquivos ansible e puppetserver

---
+++ puppetserver/production/modules/base/manifests/init.pp
@@ -0,0 +1,31 @@
+class base {
+
+case $::osfamily {
+	"Redhat": {
+		$pacotes = ["epel-release","git","vim","sysstat","htop","cowsay","figlet"]
+		$web = "httpd"
+		}
+
+	"Debian": {
+		$pacotes = ["git","vim","sysstat","htop","cowsay","figlet"]
+		$web = "apache2"
+		exec{"atualizando_pacotes":
+			command => "/usr/bin/apt update"
+		    }
+	          }	
+
+		}
+
+package 
+       {
+	$pacotes: 
+	ensure => present
+	
+       }
+
+user
+	{
+	"devops":
+	ensure => present
+	}
+}
